5b52986cb6cb874b4233a5fe4e37c03adf259356
sangermax <wonderful200x@126.com>
2019-09-18 16:50:04 +0800

    test

--- src/layouts/UserLayout.tsx
+++ src/layouts/UserLayout.tsx
@@ -1,14 +1,19 @@
-import { DefaultFooter, MenuDataItem, getMenuData, getPageTitle } from '@ant-design/pro-layout';
-import DocumentTitle from 'react-document-title';
-import Link from 'umi/link';
-import React from 'react';
-import { connect } from 'dva';
-import { formatMessage } from 'umi-plugin-react/locale';
+import {
+  DefaultFooter,
+  MenuDataItem,
+  getMenuData,
+  getPageTitle
+} from "@ant-design/pro-layout";
+import DocumentTitle from "react-document-title";
+import Link from "umi/link";
+import React from "react";
+import { connect } from "dva";
+import { formatMessage } from "umi-plugin-react/locale";
 
-import SelectLang from '@/components/SelectLang';
-import { ConnectProps, ConnectState } from '@/models/connect';
-import logo from '../assets/logo.svg';
-import styles from './UserLayout.less';
+import SelectLang from "@/components/SelectLang";
+import { ConnectProps, ConnectState } from "@/models/connect";
+import logo from "../assets/logo.svg";
+import styles from "./UserLayout.less";
 
 export interface UserLayoutProps extends ConnectProps {
   breadcrumbNameMap: { [path: string]: MenuDataItem };
@@ -17,15 +22,15 @@ export interface UserLayoutProps extends ConnectProps {
 const UserLayout: React.SFC<UserLayoutProps> = props => {
   const {
     route = {
-      routes: [],
-    },
+      routes: []
+    }
   } = props;
   const { routes = [] } = route;
   const {
     children,
     location = {
-      pathname: '',
-    },
+      pathname: ""
+    }
   } = props;
   const { breadcrumb } = getMenuData(routes);
 
@@ -35,7 +40,7 @@ const UserLayout: React.SFC<UserLayoutProps> = props => {
         pathname: location.pathname,
         breadcrumb,
         formatMessage,
-        ...props,
+        ...props
       })}
     >
       <div className={styles.container}>
@@ -44,13 +49,14 @@ const UserLayout: React.SFC<UserLayoutProps> = props => {
         </div>
         <div className={styles.content}>
           <div className={styles.top}>
+            <div className={styles.headertop}></div>
             <div className={styles.header}>
               <Link to="/">
                 <img alt="logo" className={styles.logo} src={logo} />
-                <span className={styles.title}>Ant Design</span>
+                <span className={styles.title}>ETC综合评价平台</span>
               </Link>
             </div>
-            <div className={styles.desc}>Ant Design 是西湖区最具影响力的 Web 设计规范</div>
+            <div className={styles.desc}>江苏省高速公路联网营运管理中心 </div>
           </div>
           {children}
         </div>
@@ -61,5 +67,5 @@ const UserLayout: React.SFC<UserLayoutProps> = props => {
 };
 
 export default connect(({ settings }: ConnectState) => ({
-  ...settings,
+  ...settings
 }))(UserLayout);
